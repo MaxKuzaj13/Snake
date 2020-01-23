f24716511fce8898150b62a5879bafda18573f95
MaxKuzaj13 <maksymilian.rogowski@gmail.com>
2020-01-23 13:40:22 +0100

    is_snake_bitten_tail

--- Snake.py
+++ Snake.py
@@ -13,7 +13,7 @@ bigfont = pygame.font.SysFont('monospace', 44)
 game_speed_value = 10
 black = (0, 0, 0)
 pixel_size = 20
-size_snake = (20, 20, pixel_size, pixel_size)
+size_snake = (int(res_x/2), int(res_y/2), pixel_size, pixel_size)
 count = 0
 box_color = (0, 255, 0)
 creep_direction = 90
@@ -45,7 +45,7 @@ def create_screen():
 def text_and_score(screen, score, player_name):
     disclaimer_text = myfont.render('SNAKE', 0, (255, 255, 255))
     name_text = myfont.render(f'Player: {player_name}', 0, (255, 255, 255))
-    score_text = myfont.render("Score: "+str(score), 0, (255, 255, 255))
+    score_text = myfont.render("Score: " + str(score), 0, (255, 255, 255))
     screen.blit(name_text, (10, 10))
     screen.blit(disclaimer_text, (475, 745))
     screen.blit(score_text, (10, 30))
@@ -53,8 +53,8 @@ def text_and_score(screen, score, player_name):
 
 def menu(screen):
     game = 0
-    menu_text = bigfont.render('Snake — The Game', 0, (255,255,255))
-    start_text = myfont.render('Press ENTER to start', 0, (255,255,255))
+    menu_text = bigfont.render('Snake — The Game', 0, (255, 255, 255))
+    start_text = myfont.render('Press ENTER to start', 0, (255, 255, 255))
     screen.blit(menu_text, (310, 30))
     screen.blit(start_text, (410, 600))
     pygame.display.flip()
@@ -109,15 +109,20 @@ def catch_apple(score):
 
 
 
-def is_snake_bitten_tail():
+def is_snake_bitten_tail(player_move):
+    temp_list_body_snake = player_move.copy()[1:]
+    head_snake = player_move[0]
+    if head_snake in temp_list_body_snake:
+        bool_val = True
+    else:
+        bool_val = False
+    return bool_val
 
-    return False
 
-
-def is_snake_dead(player_position, res_x, res_y):
+def is_snake_dead(player_position, res_x, res_y, player_move):
     if is_snake_in_wall(player_position, res_x, res_y):
         return True
-    elif is_snake_bitten_tail():
+    elif is_snake_bitten_tail(player_move):
         return True
     else:
         return False
@@ -133,7 +138,7 @@ def list_of_move_player(player_position, player_move, score):
 
 def inputs_from_os(creep_direction):
     temp_key = pygame.event.get()
-    if len(temp_key) ==0:
+    if len(temp_key) == 0:
         return creep_direction
     else:
         for event in temp_key:
@@ -154,7 +159,7 @@ def inputs_from_os(creep_direction):
             return creep_direction
 
 
-def direction_control(box, game_speed_value ,creep_direction):
+def direction_control(box, game_speed_value, creep_direction):
     if creep_direction == 90:
         box.x += game_speed_value
     elif creep_direction == 180:
@@ -177,8 +182,25 @@ def draw_apple_and_score(screen, apple_x, apple_y, player_name, score):
     
     '''
 
+
+def draw_the_worm(game_mode, player_move, screen, box, pixel_size):
+    if game_mode == 0:
+        # Drawing worm longer then 1
+        for i in range(len(player_move)):
+            if i == 0:
+                pygame.draw.rect(screen, (255, 0, 0), box)
+            else:
+                pygame.draw.rect(screen, (255, 0, 0), (player_move[i][0], player_move[i][1], pixel_size, pixel_size))
+    elif game_mode == 1:
+        # Drowing worm lenght 1
+        pygame.draw.rect(screen, (255, 0, 0), box)
+    else:
+        sys.exit(0)
+
+
 def main():
     # initial parameters
+    game_mode = 0
     score = 0
     game = 0
     player_move = []
@@ -190,6 +212,7 @@ def main():
     os.system('clear')
     screen = create_screen()
     while game == 0:
+        # zmienić na ardziej ituicyjne
         game = menu(screen)
     box = pygame.Rect(size_snake)
     global count
@@ -203,19 +226,12 @@ def main():
         # Clear screen
         count = clear_screen(count, screen)
         # Drawing worm
-        # Drawing worm longer then 1
-        for i in range(len(player_move)):
-            if i == 0:
-                pygame.draw.rect(screen, (255, 0, 0), box)
-            else:
-                pygame.draw.rect(screen, (255, 0, 0), (player_move[i][0], player_move[i][1], pixel_size, pixel_size))
-        # Drowing worm lenght 1
-        #pygame.draw.rect(screen, (255, 0, 0), box)
+        draw_the_worm(game_mode, player_move, screen, box, pixel_size)
         player_position = player_coordinates(box)
         player_move = list_of_move_player(player_position, player_move, score)
         # print(player_position)
         # print(is_snake_dead(player_position, res_x, res_y))
-        if is_snake_dead(player_position, res_x, res_y) == True:
+        if is_snake_dead(player_position, res_x, res_y, player_move) == True:
             sys.exit(0)
         draw_apple_and_score(screen, apple_x, apple_y, player_name, score)
 
